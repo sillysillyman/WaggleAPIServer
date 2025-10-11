abf6d6ccce4ce1e7e59759cfcf9981db337c06b1
sillysillman <sillysillyman.cs@gmail.com>
2025-10-11 19:38:07 +0900

    feat: 전역 soft delete auditing

--- src/main/kotlin/io/waggle/waggleapiserver/common/AuditingEntity.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/common/AuditingEntity.kt
@@ -3,6 +3,10 @@ package io.waggle.waggleapiserver.common
 import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
+import org.hibernate.annotations.Filter
+import org.hibernate.annotations.FilterDef
+import org.hibernate.annotations.ParamDef
+import org.hibernate.annotations.SQLDelete
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
@@ -10,6 +14,15 @@ import java.time.Instant
 
 @EntityListeners(AuditingEntityListener::class)
 @MappedSuperclass
+@SQLDelete(sql = "UPDATE #{#entityName} SET deleted_at = NOW() WHERE id = ?")
+@FilterDef(
+    name = "deletedFilter",
+    parameters = [ParamDef(name = "isDeleted", type = Boolean::class)],
+)
+@Filter(
+    name = "deletedFilter",
+    condition = "(:isDeleted = false AND deleted_at IS NULL) OR (:isDeleted = true)",
+)
 abstract class AuditingEntity {
     @CreatedDate
     @Column(nullable = false, updatable = false)
@@ -18,4 +31,6 @@ abstract class AuditingEntity {
     @LastModifiedDate
     @Column(nullable = false)
     lateinit var updatedAt: Instant
+
+    var deletedAt: Instant? = null
 }
---
+++ src/main/kotlin/io/waggle/waggleapiserver/common/aop/HibernateFilterAspect.kt
@@ -0,0 +1,22 @@
+package io.waggle.waggleapiserver.common.aop
+
+import jakarta.persistence.EntityManager
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
+import org.hibernate.Session
+import org.springframework.stereotype.Component
+
+@Aspect
+@Component
+class HibernateFilterAspect(
+    private val entityManager: EntityManager,
+) {
+    @Before(
+        "@within(org.springframework.transaction.annotation.Transactional) || @annotation(org.springframework.transaction.annotation.Transactional)",
+    )
+    fun applySoftDeleteFilter() {
+        val session = entityManager.unwrap(Session::class.java)
+        val filter = session.enableFilter("deletedFilter")
+        filter.setParameter("isDeleted", false)
+    }
+}
