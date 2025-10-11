5717aaa788d250c787f7cf54f4734c4101bd8ea2
sillysillman <sillysillyman.cs@gmail.com>
2025-10-11 15:25:48 +0900

    build: jwt version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,9 +25,9 @@ val snippetsDir = file("build/generated-snippets")
 
 dependencies {
     // JWT
-    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
     // OpenAPI (Swagger)
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
