f421c04eff13693e4905cc2080072f7d8ceb703c
sillysillman <sillysillyman.cs@gmail.com>
2025-10-13 00:14:51 +0900

    feat: 북마크 토글 구현

--- src/main/kotlin/io/waggle/waggleapiserver/domain/bookmark/BookmarkController.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/bookmark/BookmarkController.kt
@@ -1,6 +1,14 @@
 package io.waggle.waggleapiserver.domain.bookmark
 
+import io.waggle.waggleapiserver.common.util.CurrentUser
+import io.waggle.waggleapiserver.domain.bookmark.dto.request.BookmarkToggleRequest
+import io.waggle.waggleapiserver.domain.bookmark.dto.response.BookmarkToggleResponse
 import io.waggle.waggleapiserver.domain.bookmark.service.BookmarkService
+import io.waggle.waggleapiserver.domain.user.User
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class BookmarkController(
     private val bookmarkService: BookmarkService,
-)
+) {
+    @PostMapping
+    fun toggleBookmark(
+        @Valid @RequestBody request: BookmarkToggleRequest,
+        @CurrentUser user: User,
+    ): ResponseEntity<BookmarkToggleResponse> = ResponseEntity.ok(bookmarkService.toggleBookmark(request, user))
+}
--- src/main/kotlin/io/waggle/waggleapiserver/domain/bookmark/service/BookmarkService.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/bookmark/service/BookmarkService.kt
@@ -1,9 +1,40 @@
 package io.waggle.waggleapiserver.domain.bookmark.service
 
+import io.waggle.waggleapiserver.domain.bookmark.Bookmark
+import io.waggle.waggleapiserver.domain.bookmark.BookmarkId
+import io.waggle.waggleapiserver.domain.bookmark.dto.request.BookmarkToggleRequest
+import io.waggle.waggleapiserver.domain.bookmark.dto.response.BookmarkToggleResponse
 import io.waggle.waggleapiserver.domain.bookmark.repository.BookmarkRepository
+import io.waggle.waggleapiserver.domain.user.User
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class BookmarkService(
     private val bookmarkRepository: BookmarkRepository,
-)
+) {
+    @Transactional
+    fun toggleBookmark(
+        request: BookmarkToggleRequest,
+        user: User,
+    ): BookmarkToggleResponse {
+        val bookmarkId =
+            BookmarkId(
+                bookmarkableId = request.bookmarkableId,
+                bookmarkType = request.bookmarkType,
+                userId = user.id,
+            )
+        return if (bookmarkRepository.existsById(bookmarkId)) {
+            bookmarkRepository.deleteById(bookmarkId)
+            BookmarkToggleResponse(false)
+        } else {
+            val bookmark =
+                Bookmark(
+                    id = bookmarkId,
+                    user = user,
+                )
+            bookmarkRepository.save(bookmark)
+            BookmarkToggleResponse(true)
+        }
+    }
+}
