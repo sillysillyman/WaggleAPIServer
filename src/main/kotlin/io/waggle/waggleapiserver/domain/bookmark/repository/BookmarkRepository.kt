c3eae96a22a30b017d76568b71b3dfc62dedafbb
sillysillman <sillysillyman.cs@gmail.com>
2025-10-13 01:24:42 +0900

    feat: 북마크 쿼리 추가

--- src/main/kotlin/io/waggle/waggleapiserver/domain/bookmark/repository/BookmarkRepository.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/bookmark/repository/BookmarkRepository.kt
@@ -2,6 +2,18 @@ package io.waggle.waggleapiserver.domain.bookmark.repository
 
 import io.waggle.waggleapiserver.domain.bookmark.Bookmark
 import io.waggle.waggleapiserver.domain.bookmark.BookmarkId
+import io.waggle.waggleapiserver.domain.bookmark.BookmarkType
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
 
-interface BookmarkRepository : JpaRepository<Bookmark, BookmarkId>
+interface BookmarkRepository : JpaRepository<Bookmark, BookmarkId> {
+    fun countByBookmarkableIdAndBookmarkType(
+        bookmarkableId: Long,
+        bookmarkType: BookmarkType,
+    ): Int
+
+    fun findByIdUserIdAndIdBookmarkType(
+        userId: UUID,
+        bookmarkType: BookmarkType,
+    ): List<Bookmark>
+}
