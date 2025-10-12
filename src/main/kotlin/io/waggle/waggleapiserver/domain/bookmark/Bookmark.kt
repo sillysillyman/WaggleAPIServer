a1a20d49fe8903f02a823c953a6d86c6db75fba8
sillysillman <sillysillyman.cs@gmail.com>
2025-10-13 00:01:08 +0900

    refactor: AuditingEntity 상속 제거

--- src/main/kotlin/io/waggle/waggleapiserver/domain/bookmark/Bookmark.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/bookmark/Bookmark.kt
@@ -1,6 +1,5 @@
 package io.waggle.waggleapiserver.domain.bookmark
 
-import io.waggle.waggleapiserver.common.AuditingEntity
 import io.waggle.waggleapiserver.domain.user.User
 import jakarta.persistence.Column
 import jakarta.persistence.Embeddable
@@ -14,17 +13,18 @@ import jakarta.persistence.ManyToOne
 import jakarta.persistence.MapsId
 import jakarta.persistence.Table
 import java.io.Serializable
+import java.time.Instant
 import java.util.UUID
 
 @Embeddable
 data class BookmarkId(
-    @Column(name = "user_id")
+    @Column(name = "user_id", nullable = false, updatable = false)
     val userId: UUID,
-    @Column(name = "bookmarkable_id")
+    @Column(name = "bookmarkable_id", nullable = false, updatable = false)
     val bookmarkableId: Long,
     @Enumerated(EnumType.STRING)
-    @Column(name = "bookmarkable_type")
-    val bookmarkableType: BookmarkType,
+    @Column(name = "bookmark_type", nullable = false, updatable = false)
+    val bookmarkType: BookmarkType,
 ) : Serializable
 
 @Entity
@@ -34,11 +34,13 @@ class Bookmark(
     val id: BookmarkId,
     @ManyToOne(fetch = FetchType.LAZY)
     @MapsId("userId")
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", updatable = false, insertable = false)
     val user: User,
-) : AuditingEntity() {
-    val bookmarkableId get() = id.bookmarkableId
-    val bookmarkableType get() = id.bookmarkableType
+    @Column(name = "created_at", nullable = false, updatable = false)
+    val createdAt: Instant = Instant.now(),
+) {
+    val bookmarkableId: Long get() = id.bookmarkableId
+    val bookmarkType: BookmarkType get() = id.bookmarkType
 }
 
 enum class BookmarkType {
