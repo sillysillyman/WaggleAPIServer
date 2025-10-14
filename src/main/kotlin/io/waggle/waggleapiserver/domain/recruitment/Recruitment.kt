0c55baf46a9a51c01c101cba7defdd4f7ed4b234
sillysillman <sillysillyman.cs@gmail.com>
2025-10-15 00:21:11 +0900

    refactor: 인덱스 추가 및 연관관계 Post로 변경

--- src/main/kotlin/io/waggle/waggleapiserver/domain/recruitment/Recruitment.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/recruitment/Recruitment.kt
@@ -1,7 +1,7 @@
 package io.waggle.waggleapiserver.domain.recruitment
 
 import io.waggle.waggleapiserver.common.AuditingEntity
-import io.waggle.waggleapiserver.domain.project.Project
+import io.waggle.waggleapiserver.domain.post.Post
 import io.waggle.waggleapiserver.domain.user.enums.Position
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
@@ -11,12 +11,16 @@ import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Index
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 
 @Entity
-@Table(name = "recruitments")
+@Table(
+    name = "recruitments",
+    indexes = [Index(name = "idx_post_id", columnList = "post_id")],
+)
 class Recruitment(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,6 +33,6 @@ class Recruitment(
     @Column(nullable = false)
     val recruitingCount: Int,
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "project_id")
-    val project: Project,
+    @JoinColumn(name = "post_id")
+    val post: Post,
 ) : AuditingEntity()
