d1fc2321698ec27402e8acbe2c3e92da3a8ba29c
sillysillman <sillysillyman.cs@gmail.com>
2025-11-08 23:31:04 +0900

    build: flyway 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     // Database
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
 
