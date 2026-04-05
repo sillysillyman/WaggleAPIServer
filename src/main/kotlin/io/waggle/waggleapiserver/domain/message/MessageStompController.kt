b23e308ca14fb3be3028ae9319b400834fa67136
sillysillman <sillysillyman.cs@gmail.com>
2026-04-05 11:16:41 +0900

    feat(websocket-ott-auth): Principal 기반 인증 적용

--- src/main/kotlin/io/waggle/waggleapiserver/domain/message/MessageStompController.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/message/MessageStompController.kt
@@ -1,14 +1,12 @@
 package io.waggle.waggleapiserver.domain.message
 
-import io.waggle.waggleapiserver.common.exception.BusinessException
-import io.waggle.waggleapiserver.common.exception.ErrorCode
 import io.waggle.waggleapiserver.domain.message.dto.request.MessageSendRequest
 import io.waggle.waggleapiserver.domain.message.service.MessageService
-import org.springframework.messaging.handler.annotation.Header
+import io.waggle.waggleapiserver.security.oauth2.UserPrincipal
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Controller
-import java.util.UUID
+import java.security.Principal
 
 @MessageMapping("/message")
 @Controller
@@ -17,12 +15,10 @@ class MessageStompController(
 ) {
     @MessageMapping(".send")
     fun send(
-        @Header("simpSessionAttributes") attributes: Map<String, Any>,
         @Payload request: MessageSendRequest,
+        principal: Principal,
     ) {
-        val senderId =
-            attributes["userId"] as? UUID
-                ?: throw BusinessException(ErrorCode.UNAUTHORIZED, "userId not found in session")
+        val senderId = (principal as UserPrincipal).userId
         messageService.sendMessage(senderId, request)
     }
 }
