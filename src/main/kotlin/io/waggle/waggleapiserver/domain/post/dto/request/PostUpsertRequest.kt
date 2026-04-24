388bd115543832abb2d4b08e54488ba0ab6540a8
sillysillman <sillysillyman.cs@gmail.com>
2026-04-24 15:21:55 +0900

    feat(validate-request): 모집글 생성/수정 요청 DTO 유효성 검증 추가

--- src/main/kotlin/io/waggle/waggleapiserver/domain/post/dto/request/PostUpsertRequest.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/post/dto/request/PostUpsertRequest.kt
@@ -1,9 +1,12 @@
 package io.waggle.waggleapiserver.domain.post.dto.request
 
 import io.swagger.v3.oas.annotations.media.Schema
+import io.waggle.waggleapiserver.common.validation.constraint.MaxBytes
 import io.waggle.waggleapiserver.domain.recruitment.dto.request.RecruitmentUpsertRequest
+import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
 
 @Schema(description = "모집글 생성/수정 요청 DTO")
 data class PostUpsertRequest(
@@ -12,11 +15,15 @@ data class PostUpsertRequest(
     val teamId: Long,
     @Schema(description = "모집글 제목")
     @field:NotBlank
+    @field:MaxBytes(30)
     val title: String,
     @Schema(description = "모집글 내용")
     @field:NotBlank
+    @field:MaxBytes(15000)
     val content: String,
     @Schema(description = "모집 정보")
     @field:NotNull
+    @field:Size(max = 6)
+    @field:Valid
     val recruitments: List<RecruitmentUpsertRequest>,
 )
